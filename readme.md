48e7997122017cbf2c510771e90c3d377f892a51
Julian Rendell <juliangrendell@gmail.com>
2021-02-22 22:39:22 -0800

    fixes for replit and cpp vs c source

--- readme.md
+++ readme.md
@@ -2,3 +2,7 @@
 
 Copied from [https://github.com/tony/ncurses-example](https://github.com/tony/ncurses-example) becaues it had `CMake` setup, was minimal, easy to understand, and drew pretty boxes around the windows 😃
 
+## repl.it Notes
+- everything below "runner" directory will be removed
+- choosing a C++ repl from scratch defaults to the "run" button trying to compile everything
+- add a `.replit` file and have run call `cmake`. See [the minimal documentation](https://docs.repl.it/repls/dot-replit) about this feature.
---
+++ src/main.cpp
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <ncurses.h>
+
+#include "box_utils.hpp"
+#include "pixmaps.hpp"
+
+#ifndef CTRL
+#define CTRL(x) ((x)&0x1f)
+#endif
+
+int main(int argc, char* argv[]) {
+  BOX win;
+  bool quit = false;
+
+  init_box_params(&win);
+  print_box_params(&win);
+
+  initscr();
+  raw();
+  start_color();
+  cbreak();
+
+  keypad(stdscr, true);
+  noecho();
+  init_pair(1, COLOR_CYAN, COLOR_BLACK);
+
+ 
+  attron(COLOR_PAIR(1));
+  printw("Press Ctrl-C or Q to exit");
+  refresh();
+  attroff(COLOR_PAIR(1));
+
+  create_box(&win, true);
+
+  while (!quit) {
+    int ch = getch();
+    switch (ch) {
+      case 'q':
+        quit = true;
+        break;
+      case CTRL('c'):
+        quit = true;
+        break;
+      case 'j':
+        create_box(&win, false);
+        ++win.starty;
+        create_box(&win, true);
+        break;
+      case 'h':
+        create_box(&win, false);
+        --win.startx;
+        create_box(&win, true);
+        break;
+      case 'l':
+        create_box(&win, false);
+        ++win.startx;
+        create_box(&win, true);
+        break;
+      case 'k':
+        create_box(&win, false);
+        --win.starty;
+        create_box(&win, true);
+        break;
+    }
+  }
+  endwin();
+  return 0;
+}
